522f6caaa51192ef9b3c421e48a5b90833254e0c
Bhatia <anush.bhatia@capgemini.com>
2022-11-01 20:41:54 +0530

    Added the Create and Read Functionality

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
    path('', views.insert_emp, name='insert-emp'), 
    path('show/', views.show_emp, name='show-emp'), 
    path('edit/', views.edit_emp, name='edit-emp'), 
+   path('upload-csv/',views.profile_upload, name="profile_upload"),
    path('remove/', views.remove_emp, name='remove-emp')
 ]
 
--- employee_register/views.py
+++ employee_register/views.py
@@ -35,3 +35,42 @@ def edit_emp(request):
   return render(request, "employee_register\employee_list.html")
 def remove_emp(request):
   return render(request, "employee_register\employee_list.html")
+import csv, io
+from django.contrib import messages
+# Create your views here.
+# one parameter named request
+def profile_upload(request):
+      template = "employee_register\profile_upload.html"
+      data = Employee.objects.all()
+  # prompt is a context variable that can have different values      depending on their context
+      prompt = {
+          'order': 'Order of the CSV should be name, email, address,    phone, profile',
+          'profiles': data    
+                }
+      # GET request returns the value of the data with the specified key.
+      if request.method == "GET":
+          return render(request, template, prompt)
+      csv_file = request.FILES['file']
+      # let's check if it is a csv file
+      if not csv_file.name.endswith('.csv'):
+          messages.error(request, 'THIS IS NOT A CSV FILE')
+      data_set = csv_file.read().decode('UTF-8')
+      # setup a stream which is when we loop through each line we are able to handle a data in a stream
+      io_string = io.StringIO(data_set)
+      next(io_string)
+      for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        _, created = Employee.objects.update_or_create(
+        emp_code= column[0],      
+        fullname = column[1],      
+        email = column[2],    
+        primary = column[3],       
+        secondary= column[4],
+        location= column[5],
+        date= column[6],
+        remarks=column[7],
+        status=column[8],
+        designation=column[9],
+        benchmng=column[10] 
+        )
+      context = {}
+      return render(request, template, context)
