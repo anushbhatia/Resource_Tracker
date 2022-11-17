50c7852c5e45e8b6edf76fb64a4115a6c892fe11
amitmohakar <amitmohakar01@gmail.com>
2022-11-17 21:58:56 +0530

    working filter added

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
-# Create your views here.
-# def employee_list(request,template_name="employee_register\employee_list.html"): # Read
-#   return render(request, template_name)
-# def employee_form(request,template_name="employee_register\employee_form.html"): # Insert and update operations
-#   return render(request, template_name)
-# def employee_delete(request,template_name="employee_register\employee_form.html"): # Delete
-#   return render(request, template_name)
 from .models import Employee
+import csv, io
+from django.contrib import messages
+
 # Create your views here.
 def insert_emp(request,template_name="employee_register\employee_list.html"):  
   if request.method == "POST":       
@@ -25,30 +21,33 @@ def insert_emp(request,template_name="employee_register\employee_list.html"):
     secondary= secondary, location=location,date=date,remarks=remarks,status=status,designation=designation,
     benchmng=benchmng)        
     data.save()          
-    return redirect('show/')    
+    return redirect('../upload-csv/')    
   else:        
       return render(request, 'employee_register\index.html')
+
 #Home page view
-def home(request):  
+def home(request): 
   return render(request, "employee_register\home.html")
+
 def show_emp(request):  
   employees = Employee.objects.all()
   return render(request, "employee_register\data-table.html",{'employees':employees} )
+
 def edit_emp(request):  
   return render(request, "employee_register\employee_list.html")
+  
 def remove_emp(request):
   return render(request, "employee_register\employee_list.html")
 
-import csv, io
-from django.contrib import messages
+
 # Create your views here.
 # one parameter named request
 def profile_upload(request):
       template = "employee_register\profile_upload.html"
       data = Employee.objects.all()
-  # prompt is a context variable that can have different values      depending on their context
+      # prompt is a context variable that can have different values depending on their context
       prompt = {
-          'order': 'Order of the CSV should be name, email, address,    phone, profile',
+          'order': 'Order of the CSV should be name, email, address, phone, profile',
           'profiles': data    
                 }
       # GET request returns the value of the data with the specified key.
@@ -58,6 +57,7 @@ def profile_upload(request):
       # let's check if it is a csv file
       if not csv_file.name.endswith('.csv'):
           messages.error(request, 'THIS IS NOT A CSV FILE')
+          return redirect('../upload-csv/')
       data_set = csv_file.read().decode('UTF-8')
       # setup a stream which is when we loop through each line we are able to handle a data in a stream
       io_string = io.StringIO(data_set)
@@ -78,6 +78,9 @@ def profile_upload(request):
         )
       context = {}
       return render(request, template, context)
+
+       
+      
 
 
   
--- static/showAsset/excel-bootstrap-table-filter-bundle.js
+++ static/showAsset/excel-bootstrap-table-filter-bundle.js
@@ -1,4 +1,3 @@
- console.log('Hello');
 (function ($$1) {
 'use strict';
 
