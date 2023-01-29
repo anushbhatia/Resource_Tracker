820d2384fcf95d4b2d156e65a8127d697ae4cc29
Anush Bhatia <anushbhatia1234@gmail.com>
2023-01-29 23:12:35 +0530

    Update settings.py

--- resource_tracker/settings.py
+++ resource_tracker/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anushbhatia1234@gmail.com'
-EMAIL_HOST_PASSWORD = 'iodn hfae syxu xmic'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
